559345bdaa59a7c6b478f1147fea528618a22104
FAUGERON Guillaume <guillaume.faugeron@ynov.com>
2021-05-25 17:17:21 +0200

    kdo

---
+++ Debug/main.cpp.o.d
@@ -0,0 +1,126 @@
+Debug/main.cpp.o: main.cpp C:\SFML\include/SFML/Window.hpp \
+ C:\SFML\include/SFML/System.hpp C:\SFML\include/SFML/Config.hpp \
+ C:\SFML\include/SFML/System/Clock.hpp \
+ C:\SFML\include/SFML/System/Export.hpp \
+ C:\SFML\include/SFML/System/Time.hpp C:\SFML\include/SFML/System/Err.hpp \
+ C:\SFML\include/SFML/System/FileInputStream.hpp \
+ C:\SFML\include/SFML/System/InputStream.hpp \
+ C:\SFML\include/SFML/System/NonCopyable.hpp \
+ C:\SFML\include/SFML/System/Lock.hpp \
+ C:\SFML\include/SFML/System/MemoryInputStream.hpp \
+ C:\SFML\include/SFML/System/Mutex.hpp \
+ C:\SFML\include/SFML/System/Sleep.hpp \
+ C:\SFML\include/SFML/System/String.hpp \
+ C:\SFML\include/SFML/System/Utf.hpp C:\SFML\include/SFML/System/Utf.inl \
+ C:\SFML\include/SFML/System/String.inl \
+ C:\SFML\include/SFML/System/Thread.hpp \
+ C:\SFML\include/SFML/System/Thread.inl \
+ C:\SFML\include/SFML/System/ThreadLocal.hpp \
+ C:\SFML\include/SFML/System/ThreadLocalPtr.hpp \
+ C:\SFML\include/SFML/System/ThreadLocalPtr.inl \
+ C:\SFML\include/SFML/System/Vector2.hpp \
+ C:\SFML\include/SFML/System/Vector2.inl \
+ C:\SFML\include/SFML/System/Vector3.hpp \
+ C:\SFML\include/SFML/System/Vector3.inl \
+ C:\SFML\include/SFML/Window/Clipboard.hpp \
+ C:\SFML\include/SFML/Window/Export.hpp \
+ C:\SFML\include/SFML/Window/Context.hpp \
+ C:\SFML\include/SFML/Window/GlResource.hpp \
+ C:\SFML\include/SFML/Window/ContextSettings.hpp \
+ C:\SFML\include/SFML/Window/Cursor.hpp \
+ C:\SFML\include/SFML/Window/Event.hpp \
+ C:\SFML\include/SFML/Window/Joystick.hpp \
+ C:\SFML\include/SFML/Window/Keyboard.hpp \
+ C:\SFML\include/SFML/Window/Mouse.hpp \
+ C:\SFML\include/SFML/Window/Sensor.hpp \
+ C:\SFML\include/SFML/Window/Touch.hpp \
+ C:\SFML\include/SFML/Window/VideoMode.hpp \
+ C:\SFML\include/SFML/Window/Window.hpp \
+ C:\SFML\include/SFML/Window/WindowHandle.hpp \
+ C:\SFML\include/SFML/Window/WindowStyle.hpp
+
+C:\SFML\include/SFML/Window.hpp:
+
+C:\SFML\include/SFML/System.hpp:
+
+C:\SFML\include/SFML/Config.hpp:
+
+C:\SFML\include/SFML/System/Clock.hpp:
+
+C:\SFML\include/SFML/System/Export.hpp:
+
+C:\SFML\include/SFML/System/Time.hpp:
+
+C:\SFML\include/SFML/System/Err.hpp:
+
+C:\SFML\include/SFML/System/FileInputStream.hpp:
+
+C:\SFML\include/SFML/System/InputStream.hpp:
+
+C:\SFML\include/SFML/System/NonCopyable.hpp:
+
+C:\SFML\include/SFML/System/Lock.hpp:
+
+C:\SFML\include/SFML/System/MemoryInputStream.hpp:
+
+C:\SFML\include/SFML/System/Mutex.hpp:
+
+C:\SFML\include/SFML/System/Sleep.hpp:
+
+C:\SFML\include/SFML/System/String.hpp:
+
+C:\SFML\include/SFML/System/Utf.hpp:
+
+C:\SFML\include/SFML/System/Utf.inl:
+
+C:\SFML\include/SFML/System/String.inl:
+
+C:\SFML\include/SFML/System/Thread.hpp:
+
+C:\SFML\include/SFML/System/Thread.inl:
+
+C:\SFML\include/SFML/System/ThreadLocal.hpp:
+
+C:\SFML\include/SFML/System/ThreadLocalPtr.hpp:
+
+C:\SFML\include/SFML/System/ThreadLocalPtr.inl:
+
+C:\SFML\include/SFML/System/Vector2.hpp:
+
+C:\SFML\include/SFML/System/Vector2.inl:
+
+C:\SFML\include/SFML/System/Vector3.hpp:
+
+C:\SFML\include/SFML/System/Vector3.inl:
+
+C:\SFML\include/SFML/Window/Clipboard.hpp:
+
+C:\SFML\include/SFML/Window/Export.hpp:
+
+C:\SFML\include/SFML/Window/Context.hpp:
+
+C:\SFML\include/SFML/Window/GlResource.hpp:
+
+C:\SFML\include/SFML/Window/ContextSettings.hpp:
+
+C:\SFML\include/SFML/Window/Cursor.hpp:
+
+C:\SFML\include/SFML/Window/Event.hpp:
+
+C:\SFML\include/SFML/Window/Joystick.hpp:
+
+C:\SFML\include/SFML/Window/Keyboard.hpp:
+
+C:\SFML\include/SFML/Window/Mouse.hpp:
+
+C:\SFML\include/SFML/Window/Sensor.hpp:
+
+C:\SFML\include/SFML/Window/Touch.hpp:
+
+C:\SFML\include/SFML/Window/VideoMode.hpp:
+
+C:\SFML\include/SFML/Window/Window.hpp:
+
+C:\SFML\include/SFML/Window/WindowHandle.hpp:
+
+C:\SFML\include/SFML/Window/WindowStyle.hpp:
---
+++ test.mk
@@ -0,0 +1,110 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=test
+ConfigurationName      :=Debug
+WorkspacePath          :=C:/Users/guillaume/Documents/cours_cpp
+ProjectPath            :=C:/Users/guillaume/Documents/cours_cpp/test
+IntermediateDirectory  :=$(ConfigurationName)
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=guillaume
+Date                   :=25/05/2021
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :="C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/g++.exe"
+SharedObjectLinkerName :="C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/g++.exe" -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="test.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :="C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/windres.exe"
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch)C:\SFML\include 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := $(LibrarySwitch)sfml-graphics-d $(LibrarySwitch)sfml-window-d $(LibrarySwitch)sfml-system-d $(LibrarySwitch)sfml-graphics $(LibrarySwitch)sfml-window $(LibrarySwitch)sfml-system 
+ArLibs                 :=  "sfml-graphics-d" "sfml-window-d" "sfml-system-d" "sfml-graphics" "sfml-window" "sfml-system" 
+LibPath                := $(LibraryPathSwitch). $(LibraryPathSwitch)C:\SFML\lib 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/ar.exe" rcu
+CXX      := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/g++.exe"
+CC       := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/gcc.exe"
+CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin/as.exe"
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Program Files\CodeLite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "$(ConfigurationName)"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "$(ConfigurationName)"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/guillaume/Documents/cours_cpp/test/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r $(ConfigurationName)/
+
+
